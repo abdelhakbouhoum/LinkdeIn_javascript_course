d8f6d523ca4c03f70bdd7317b0aea9f13f307128
Abdelhak BOUHOUM <abdelhakbouhoum@gmail.com>
2022-08-23 22:22:21 +0100

    Practice 03_07

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,28 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const car = {
+  name: "Car",
+  matricul: "123456-A-2",
+  brand: "Reanault",
+  model: "Megane",
+  placesNbr: 5,
+  color: "black",
+  motor: {
+    type: "Gazoil",
+    horseNbr: 6,
+  },
+};
+
+const client = {
+  name: "Client",
+  id: "AB262626",
+  firstName: "Abdelhak",
+  lastName: "BOUHOUM",
+  gender: "Male",
+  age: 30,
+  nationality: "Moroccan",
+  bornDate: "24/09/1983",
+  Married: true,
+};
